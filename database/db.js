d793bb958d6a2e5bcf78a1d3d300392227aebd2e
achyut vardhan <achyutvardhan234@gmail.com>
2023-06-29 02:23:27 +0530

    connected to mongoose

---
+++ database/db.js
@@ -0,0 +1,103 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    id_no: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    alt_email:{
+        type: String,
+    }
+    // Add other relevant user fields here
+  });
+  
+  // UserDetails Schema
+  const userDetailsSchema = new mongoose.Schema({
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      unique: true
+    },
+    full_name: {
+      type: String,
+      required: true
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ['Male', 'Female', 'Other'],
+      required: true,
+    },
+    profession:{
+        type:String,
+        enum:['Advocate','CA'],
+        required: true,
+    },
+    father_name : {
+        type: String,
+        required: true
+    },
+    marital_status:{
+        type:String,
+        enum:['married','unmarried'],
+        required: true,
+    },
+    marital_date:{
+        type:Date,
+    },
+    phone_no: {
+        type: Number,
+        required: true,
+      },
+      alt_phone_no:{
+        type: Number
+      }
+    // Add other relevant user details fields here
+  });
+  
+  // Message Schema
+  const messageSchema = new mongoose.Schema({
+    sender_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    receiver_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    timestamp: {
+      type: Date,
+      default: Date.now
+    }
+  });
+  
+  const User = mongoose.model('User', userSchema);
+  const UserDetails = mongoose.model('UserDetails', userDetailsSchema);
+  const Message = mongoose.model('Message', messageSchema);
+  
+  // Create models based on the schemas
+module.exports = {
+  User,UserDetails,Message
+}
--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app =  express()
 const cors = require('cors')
-
+const mongoose =  require('mongoose')
+const {User,Message,UserDetails} = require('./database/db')
+app.use(express.json());
 app.use(cors());
+
+mongoose.connect("mongodb://localhost:27017/chatok");
+
+const db = mongoose.connection;
+db.on('error', (error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+  db.once('open',()=>{
+    console.log("Connected to mongodb")
+  })
+
+
 
 app.get('*',(req,res)=>{
     res.send("page doesn't exist");
